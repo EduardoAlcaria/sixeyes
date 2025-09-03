ced38c1049ad641e1d5b01e7e8cd2f3483fc1194
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-09-03 13:39:12 -0300

    DTO Class for the magnet

---
+++ java/controller/src/main/java/com/sixeyes/controller/DTO.java
@@ -0,0 +1,13 @@
+package com.sixeyes.controller;
+
+public class DTO {
+    private String magnet;
+
+    public String getMagnet() {
+        return magnet;
+    }
+
+    public void setMagnet(String magnet) {
+        this.magnet = magnet;
+    }
+}
