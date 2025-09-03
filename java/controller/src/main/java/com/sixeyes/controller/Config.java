c41748e30f4032e9e3d2842f3475c65f57990c73
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-09-03 13:38:54 -0300

    Config File for the CORS

---
+++ java/controller/src/main/java/com/sixeyes/controller/Config.java
@@ -0,0 +1,32 @@
+package com.sixeyes.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()      // disable CSRF for testing
+                .authorizeHttpRequests((auth) -> auth.anyRequest().permitAll()); // allow all
+        return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer configurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("POST", "GET", "PUT", "DELETE");
+            }
+        };
+    }
+}
