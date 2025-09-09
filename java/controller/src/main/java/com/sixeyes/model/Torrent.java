d0ef1724fcc0b9d6daadc82a2bf2384b127aad70
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-09-09 10:25:32 -0300

    start downloading and getting the torrent info working

---
+++ java/controller/src/main/java/com/sixeyes/model/Torrent.java
@@ -0,0 +1,163 @@
+package com.sixeyes.model;
+
+
+
+import jakarta.persistence.*;
+import org.libtorrent4j.TorrentHandle;
+
+
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "Torrent")
+
+public class Torrent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, unique = true, length = 4000)
+    private String magnet;
+
+    @Column(nullable = false)
+    private String infoHash;
+
+
+
+
+    private Double progress = 0.0;
+    private String downloadSpeed = "0 MB/s";
+    private String uploadSpeed = "0 MB/s";
+    private Integer peers = 0;
+    private String eta;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public Torrent() {}
+
+    @Override
+    public String toString() {
+        return "Torrent{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", magnet='" + magnet + '\'' +
+                ", infoHash='" + infoHash + '\'' +
+                ", progress=" + progress +
+                ", downloadSpeed='" + downloadSpeed + '\'' +
+                ", uploadSpeed='" + uploadSpeed + '\'' +
+                ", peers=" + peers +
+                ", eta='" + eta + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+
+    public Torrent(String title, String magnet, String infoHash) {
+        this.title = title;
+        this.magnet = magnet;
+        this.infoHash = infoHash;
+    }
+
+    public Torrent(String magnet) {
+        this.magnet = magnet;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMagnet() {
+        return magnet;
+    }
+
+    public void setMagnet(String magnet) {
+        this.magnet = magnet;
+    }
+
+    public String getInfoHash() {
+        return infoHash;
+    }
+
+    public void setInfoHash(String infoHash) {
+        this.infoHash = infoHash;
+    }
+
+
+    public Double getProgress() {
+        return progress;
+    }
+
+    public void setProgress(Double progress) {
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        this.progress = Double.parseDouble(decimalFormat.format(progress));
+    }
+
+    public String getDownloadSpeed() {
+        return downloadSpeed;
+    }
+
+    public void setDownloadSpeed(String downloadSpeed) {
+        this.downloadSpeed = downloadSpeed;
+    }
+
+    public String getUploadSpeed() {
+        return uploadSpeed;
+    }
+
+    public void setUploadSpeed(String uploadSpeed) {
+        this.uploadSpeed = uploadSpeed;
+    }
+
+    public Integer getPeers() {
+        return peers;
+    }
+
+    public void setPeers(Integer peers) {
+        this.peers = peers;
+    }
+
+    public String getEta() {
+        return eta;
+    }
+
+    public void setEta(String eta) {
+        this.eta = eta;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
