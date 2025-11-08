6665eade5cae2baba586c4ede44670655735ece4
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-11-08 16:33:37 -0300

    improved the getInfoPython impleted the restTemplate obj

--- java/controller/src/main/java/com/sixeyes/controller/GetInfoPython.java
+++ java/controller/src/main/java/com/sixeyes/controller/GetInfoPython.java
@@ -1,9 +1,10 @@
 package com.sixeyes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -11,15 +12,22 @@ public class GetInfoPython {
 
     @Value("${python.service.url}")
     private String pythonUrl;
-    private final RestTemplate restTemplate = new RestTemplate();
 
-    public String getStorage(){
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public GetInfoPython(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String getStorage() {
         String flaskURL = pythonUrl + "/python/systemInfo/getStorageInfo";
         try {
             ResponseEntity<String> response = restTemplate.getForEntity(flaskURL, String.class);
             return response.getBody();
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            System.out.println("Failed to get storage from Flask: " + e.getMessage());
+            throw new RuntimeException("Flask storage request failed", e);
         }
     }
 }
