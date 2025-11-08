50963b0f1deb8f923e4ce1ff5027c42343215b2a
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-11-08 14:30:30 -0300

    added a restTemplate template with a pre programed TO

--- java/controller/src/main/java/com/sixeyes/controller/Config.java
+++ java/controller/src/main/java/com/sixeyes/controller/Config.java
@@ -8,6 +8,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
 @Configuration
 @EnableWebMvc
 public class Config implements WebMvcConfigurer {
@@ -43,5 +50,21 @@ public class Config implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+
+
+
+
+    @Configuration
+    public class RestTemplateConfig {
+
+        @Bean
+        public RestTemplate restTemplate(RestTemplateBuilder builder) {
+            return builder
+                    .setConnectTimeout(Duration.ofSeconds(3))
+                    .setReadTimeout(Duration.ofSeconds(5))
+                    .build();
+        }
     }
 }
