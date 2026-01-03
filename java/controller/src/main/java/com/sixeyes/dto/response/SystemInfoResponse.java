1a04fb953977822c5f91c5eaf5f6e549031c7935
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2026-01-03 12:55:38 -0300

    feat(system): add device to storage info

--- java/controller/src/main/java/com/sixeyes/dto/response/SystemInfoResponse.java
+++ java/controller/src/main/java/com/sixeyes/dto/response/SystemInfoResponse.java
@@ -2,6 +2,6 @@ package com.sixeyes.dto.response;
 
 public record SystemInfoResponse(StorageInfo storage, NetworkInfo network) {
 
-    public record StorageInfo(double total, double used, double available) {}
+    public record StorageInfo(double total, double used, double available, String device) {}
     public record NetworkInfo(double downloadSpeed, double uploadSpeed) {}
 }
