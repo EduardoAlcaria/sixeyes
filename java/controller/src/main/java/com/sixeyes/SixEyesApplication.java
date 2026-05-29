fbbba6333822aac2e8621dfe00d737d4ef8fce82
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2026-05-29 14:17:25 -0300

    chore(baseline): add java/controller/src/main/java/com/sixeyes/SixEyesApplication.java

---
+++ java/controller/src/main/java/com/sixeyes/SixEyesApplication.java
@@ -0,0 +1,14 @@
+package com.sixeyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@EnableScheduling
+public class SixEyesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SixEyesApplication.class, args);
+    }
+}
