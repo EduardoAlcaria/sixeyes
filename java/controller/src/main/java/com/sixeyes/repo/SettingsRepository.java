5cbf199d1b35a05fc6d87bbf9f16391f899d9fba
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2026-05-29 11:31:50 -0300

    chore(baseline): add java/controller/src/main/java/com/sixeyes/repo/SettingsRepository.java

---
+++ java/controller/src/main/java/com/sixeyes/repo/SettingsRepository.java
@@ -0,0 +1,8 @@
+package com.sixeyes.repo;
+
+import com.sixeyes.model.Settings;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SettingsRepository extends JpaRepository<Settings, Long> {}
