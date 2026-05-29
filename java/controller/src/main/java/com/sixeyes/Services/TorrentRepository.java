9af5e99cf9a691b2227c598c64aa0bb08efde463
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2026-05-29 11:31:39 -0300

    chore(baseline): remove java/controller/src/main/java/com/sixeyes/Services/TorrentRepository.java

--- java/controller/src/main/java/com/sixeyes/Services/TorrentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sixeyes.Services;
-
-import com.sixeyes.model.Torrent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TorrentRepository extends JpaRepository<Torrent, Long> {
-    Torrent findByStatus(Torrent torrent);
-}
