2990a4c2832dd0068b4ba0e2fbc528dbec565f06
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2026-06-04 22:06:32 -0300

    refactor: removed some comments

--- java/controller/src/main/java/com/sixeyes/dto/request/InstallStatusRequest.java
+++ java/controller/src/main/java/com/sixeyes/dto/request/InstallStatusRequest.java
@@ -3,7 +3,6 @@ package com.sixeyes.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-/** Host watcher reports install progress/result back to the API. */
 public record InstallStatusRequest(
 
         @NotBlank(message = "status is required")
