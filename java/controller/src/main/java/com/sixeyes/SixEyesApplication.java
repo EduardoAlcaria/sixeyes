8e17d87322a9375397bc510331a48584b6f345dc
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2026-06-15 20:23:51 -0300

    feat: add @EnableAsync to SixEyesApplication

--- java/controller/src/main/java/com/sixeyes/SixEyesApplication.java
+++ java/controller/src/main/java/com/sixeyes/SixEyesApplication.java
@@ -3,9 +3,11 @@ package com.sixeyes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@EnableAsync
 @EnableScheduling
 public class SixEyesApplication {
     public static void main(String[] args) {
